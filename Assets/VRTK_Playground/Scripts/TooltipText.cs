914e59311a7654bbea81945990dc9179a4c900f8
George Zhou <georgezhou89@hotmail.com>
2023-01-17 14:16:40 -0500

    Added BG

---
+++ Assets/VRTK_Playground/Scripts/TooltipText.cs
@@ -0,0 +1,25 @@
+﻿using Tilia.Visuals.Tooltip;
+using UnityEngine;
+using Zinnia.Utility;
+
+public class TooltipText : MonoBehaviour
+{
+    public TooltipFacade toolTipFacade;
+
+    public void ShowToolTip(float val)
+    {
+        //toolTipFacade.TooltipText = string.Concat("Slider Value : ", val);
+        toolTipFacade.TooltipText = val.ToString("f0");
+    }
+
+    public void ShowToolTip(GameObject go) //By declaring the methods parameter as a game object, this method will show up as a dynamic method, when used as part of events of a facade component. 
+    {
+        toolTipFacade.TooltipText = go.tag;
+    }
+
+    public void ClearToolTip()
+    {
+        toolTipFacade.TooltipText = string.Empty;
+    }
+
+}
